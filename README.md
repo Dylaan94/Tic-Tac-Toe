e18b0bc96ca300bb74c24bcbd2f52c3f088e7b0d
Dylan Brain <dylanbrain@googlemail.com>
2021-02-16 19:41:41 +0900

    further func with modules

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Tic-Tac-Toe
-Tic-Tac-Toe for the Odin Project
+<b>Tic-Tac-Toe for the Odin Project </b>
+
+<br> Things I've learnt (constantly updating)
+<br> First project using ES6
---
+++ script.js
@@ -0,0 +1,105 @@
+// module
+// if you only need one of something, use a module
+// remember to add parenthesis at the end of a module
+
+const gameBoard = (() => {
+    let gameDisplay = document.getElementById("gameDisplay");
+    let gameBoardArr = [];
+    let newDiv; 
+
+    // creates grid for gameboard
+
+    for (i = 0; i < 9; i++) {
+        newDiv = document.createElement("div");    
+        newDiv.className = "square";
+        newDiv.id = i;
+        newDiv.innerHTML = "O"
+        gameBoardArr.push(newDiv);
+        gameDisplay.appendChild(newDiv);
+    }
+   
+    console.log(gameDisplay);
+    console.log(gameBoardArr);
+
+    const getGameDisplay = () => {
+        return gameBoardArr;
+    }
+
+    let selection = () => {
+
+    }
+
+    // make values available to other modules
+    // call as function ();
+    return {
+        getGameDisplay
+    }
+
+})();
+
+const displayController = (() => {
+    let gameBoardArr = gameBoard.getGameDisplay();
+    let checked;
+    console.log(gameBoard.getGameDisplay());
+    console.log(gameBoardArr);
+
+    // checks if div has been clicked 
+
+        gameBoardArr.forEach(gameBoardArr => {
+            gameBoardArr.addEventListener("click", () => {
+                let index = gameBoardArr.id;
+                console.log("clicked " + index)
+                gameBoardArr.innerHTML = "X"
+                checked = gameBoardArr.id;
+                console.log(checked)
+                checkFunc()
+            })
+            
+        })
+
+        let checkFunc = () => {
+            return checked;
+        }
+
+        let testFunc = () => {
+            return "hello"
+        }
+
+    return {
+        checkFunc,
+        testFunc
+    }
+})();
+
+
+
+const players = (() => {
+    let player1 =  {
+        name: "Player 1",
+        marker: "X",
+        selection: []
+    }
+
+    let player2 = {
+        name: "Player 2",
+        marker: "O",
+        selection: []
+    }
+console.log("Player 1 selection is " + player1.selection)
+console.log("Player 2 selection is " + player2.selection)
+
+return {
+    player1,
+    player2,
+}
+
+})();
+
+const test = '';
+
+
+const playGame = () => {
+
+
+
+}
